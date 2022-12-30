7bd9050dad93741bf3bf7bbe507f98933399d1e7
Hocorend <hocorend@gmail.com>
2022-12-30 17:35:08 +0500

    SubHunter

---
+++ SubHunter/app/src/main/java/com/gamecodeschool/subhunter/SubHunter.java
@@ -0,0 +1,42 @@
+package com.gamecodeschool.subhunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+public class SubHunter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    void newGame(){
+
+    }
+
+    void draw(){
+
+    }
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        
+
+    }
+
+    void  takeShot(){
+
+    }
+
+    void boom(){
+
+    }
+
+    void printDebuggingText(){
+
+    }
+
+}
